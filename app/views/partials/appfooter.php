f9f8d6e81df72cf0e032d13958d5f8909c8ac22b
baihaqidawanis <dawanisbaihaqi@gmail.com>
2026-09-14 11:26:33 +0700

    style(ui): update full developer name in footer to Muhammad Baihaqi Dawanis

--- app/views/partials/appfooter.php
+++ app/views/partials/appfooter.php
@@ -2,9 +2,10 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-12 text-center text-muted">
-				<div class="copyright py-2">All Rights Reserved | &copy; <?php echo SITE_NAME ?> - <?php echo date('Y') ?> | Developed by M. Baihaqi Dawanis</div>
+				<div class="copyright py-2">All Rights Reserved | &copy; <?php echo SITE_NAME ?> -
+					<?php echo date('Y') ?> | Developed by Muhammad Baihaqi Dawanis</div>
 			</div>
-			
+
 		</div>
 	</div>
 </footer>
